9e6a590566dbeaa2c17df766c0d762635c7e1231
c212 <40395648+c212@users.noreply.github.com>
2024-09-19 11:55:27 -0400

    Update Numbers.java

--- labs/lab04/Numbers.java
+++ labs/lab04/Numbers.java
@@ -1,5 +1,5 @@
 public class Numbers {
-  static int nextAvailableNumber = 6;
+  static int nextAvailableNumber = 23;
   int id = 0;
   private Numbers() {
     this.id = Numbers.nextAvailableNumber++;
@@ -12,5 +12,9 @@ public class Numbers {
   }
   public String toString() {
     return "I am number: " + this.id;
+  }
+  public static void main(String[] args) {
+    One a = new One();
+    a.start();
   }
 }
